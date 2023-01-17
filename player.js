67117d5996eb930fa9cb4dad456f66d55cc8bb57
Kara Anderson <kanderson8585@gmail.com>
2023-01-17 14:11:45 -0700

    Fix indentation and check syntax

--- player.js
+++ player.js
@@ -1,20 +1,20 @@
 class Player {
-    constructor(name, token) {
-       this.name = name;
-       this.token = token;
-       this.wins = 0;
-       this.chosenFighter; 
+  constructor(name, token) {
+    this.name = name;
+    this.token = token;
+    this.wins = 0;
+    this.chosenFighter; 
+  }
+  takeTurn(chosenImg) {
+    if(this.name === 'cabbage merchant') {
+      this.chosenFighter = parseInt(chosenImg);
+    } else if(currentGame.selectedGame === 'classic' && this.name === 'avatar state') {
+      this.getRandomFighter(0, 3);
+    } else if(currentGame.selectedGame === 'complex' && this.name === 'avatar state') {
+      this.getRandomFighter(3, 8);
     }
-    takeTurn(chosenImg) {
-        if(this.name === 'cabbage merchant') {
-            this.chosenFighter = parseInt(chosenImg);
-        } else if(currentGame.selectedGame === 'classic' && this.name === 'avatar state') {
-            this.getRandomFighter(0, 3);
-        } else if(currentGame.selectedGame === 'complex' && this.name === 'avatar state') {
-            this.getRandomFighter(3, 8);
-        }
-    }
-    getRandomFighter(min, max) {
-        this.chosenFighter = currentGame.fighters[Math.floor(Math.random() * (max - min))];
-    }
+  }
+  getRandomFighter(min, max) {
+    this.chosenFighter = currentGame.fighters[Math.floor(Math.random() * (max - min))];
+  }
 }
